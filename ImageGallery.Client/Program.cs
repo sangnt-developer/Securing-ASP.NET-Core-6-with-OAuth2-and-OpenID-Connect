a95394b0bb2aa1ea50ff904b17884ac7d47a4308
SangNguyenAcerNitro <SangNguyenAcerNitro@DESKTOP-8I87CV8>
2023-02-13 22:30:47 +0700

    Add project files.

---
+++ ImageGallery.Client/Program.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Net.Http.Headers;
+using System.IdentityModel.Tokens.Jwt;
+using static System.Net.WebRequestMethods;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(configure => 
+        configure.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+// Make sure keeping the orginal claim types.
+// Not transform it to other different claim types based on WS-Security (Microsoft).
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+// create an HttpClient used for accessing the API
+builder.Services.AddHttpClient("APIClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ImageGalleryAPIRoot"]);
+    client.DefaultRequestHeaders.Clear();
+    client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.AccessDeniedPath = new PathString("/Authentication/AccessDenied");
+})
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.Authority = "https://localhost:5001";
+    options.ClientId = "imagegalleryclient";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+
+    // these scopes is requested by middleware
+    //options.Scope.Add("openid");
+    //options.Scope.Add("profile");
+
+    // these "signin-oidc" callbackPath is default path of middleware
+    //options.CallbackPath = new PathString("signin-oidc");
+
+    // SignedOutCallbackPath: default = host:port/signout-callback-oidc
+    // To change, set SignedOutCallbackPath
+    // eg: SignoutCallbackPath = "pathaftersignout"
+
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.SaveTokens = true;
+
+    // Remove claim actiosn on "aud" claim type
+    options.ClaimActions.Remove("aud");
+
+    // Delete claims types
+    options.ClaimActions.DeleteClaim("sid");
+    options.ClaimActions.DeleteClaim("idp");
+
+    options.Scope.Add("roles");
+    options.ClaimActions.MapJsonKey("role", "role");
+    options.TokenValidationParameters = new ()
+    {
+        NameClaimType = "given_name",
+        RoleClaimType = "role"
+    };
+});
+
+var app = builder.Build();
+
+// ure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Gallery}/{action=Index}/{id?}");
+
+app.Run();
